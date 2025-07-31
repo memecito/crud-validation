0255cff48c4b06532a62477d8294f49a392cd3fd
memecito <memecito@users.noreply.github.com>
2025-07-31 19:52:16 +0200

    inicio proyecto

---
+++ src/main/java/es/nter/crud_validation/CrudValidationApplication.java
@@ -0,0 +1,13 @@
+package es.nter.crud_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/nter/crud_validation/CrudValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package es.nter.crud_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
